0af674904f651caf45507fddd37220caef20881b
jhamadhav <jhamadhav28@gmail.com>
2022-06-02 20:27:39 +0530

    D4 REVERSE py added

---
+++ syllabus-1/day-4/REVERSE.py
@@ -0,0 +1,50 @@
+import heapq
+
+
+def dfs(st, arr):
+    costs = [-1 for i in range(len(arr))]
+    visited = [0 for i in range(len(arr))]
+
+    q = []
+    heapq.heappush(q, (0, st))
+    costs[st] = 0
+
+    while(len(q) > 0):
+        temp = heapq.heappop(q)
+        val = temp[0]
+        node = temp[1]
+
+        if(visited[node] == 1):
+            continue
+
+        visited[node] = 1
+        for i in arr[node]:
+            if(visited[i[0]] != 0):
+                continue
+
+            if(costs[i[0]] == -1):
+                costs[i[0]] = i[1] + val
+            else:
+                costs[i[0]] = min(costs[i[0]], i[1] + val)
+            heapq.heappush(q, (costs[i[0]], i[0]))
+    return costs
+
+
+def solve():
+    (n, e) = map(int, input().split())
+
+    arr = [[] for i in range(n)]
+    for i in range(e):
+        (a, b) = map(int, input().split())
+        a -= 1
+        b -= 1
+
+        arr[a].append((b, 0))
+        arr[b].append((a, 1))
+
+    costs = dfs(0, arr)
+
+    print(costs[n-1])
+
+
+solve()
